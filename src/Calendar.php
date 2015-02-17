d8d074ea8500b9a8e90761b9a52ee9a30ff0a1d1
Florent Viel <florent.viel69@gmail.com>
2015-02-17 14:49:53 +0100

    restrict objects type that can pe added to a calendar

--- src/Calendar.php
+++ src/Calendar.php
@@ -1,11 +1,23 @@
 <?php
 namespace Calendar;
 
+use Calendar\PeriodInterface;
 use Doctrine\Common\Collections\ArrayCollection;
+use UnexpectedValueException;
 
 /**
  * @author Florent Viel <florent.viel69@gmail.com>
  */
 class Calendar extends ArrayCollection implements CalendarInterface
 {
+    public function add($period)
+    {
+        if (!$period instanceof PeriodInterface) {
+            throw new UnexpectedValueException('Each period must implement Calender\\PeriodInterface.');
+        }
+
+        parent::add($period);
+
+        return true;
+    }
 }
